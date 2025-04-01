c610b14f2e7494897870355bd0ebe2e82c6f174f
rogue-35 <cartercet@gmail.com>
2025-03-31 19:35:31 -0500

    Renamed to TooFaster

--- src/main/java/com/example/removespeedcap/RemoveSpeedCapMod.java
+++ src/main/java/com/example/removespeedcap/RemoveSpeedCapMod.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RemoveSpeedCapMod implements ModInitializer {
-    public static final String MOD_ID = "remove-speed-cap";
+    public static final String MOD_ID = "TooFaster";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override

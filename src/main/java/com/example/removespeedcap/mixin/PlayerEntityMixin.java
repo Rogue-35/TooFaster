9462cf4947a8bd7812570fbd69814ea402ba0931
Rogue-35 <cartercet@gmail.com>
2025-12-01 22:55:45 -0500

    fixed edge case errors

--- src/main/java/com/example/removespeedcap/mixin/PlayerEntityMixin.java
+++ src/main/java/com/example/removespeedcap/mixin/PlayerEntityMixin.java
@@ -7,21 +7,19 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class PlayerEntityMixin {
-    private static final float VERY_HIGH_SPEED = 1000000.0F;
-    private static final double VERY_HIGH_SPEED_DOUBLE = 1000000.0;
 
-    @ModifyConstant(method = "processPlayer", constant = @Constant(floatValue = 100.0F))
-    private float removePlayerSpeedLimit(float original) {
-        return VERY_HIGH_SPEED;
+    @ModifyConstant(method = "*", constant = @Constant(floatValue = 100.0F), require = 0)
+    private float getDefaultMaxPlayerSpeed(float speed) {
+        return Float.MAX_VALUE;
     }
 
-    @ModifyConstant(method = "processPlayer", constant = @Constant(floatValue = 300.0F))
-    private float removeElytraSpeedLimit(float original) {
-        return VERY_HIGH_SPEED;
+    @ModifyConstant(method = "*", constant = @Constant(floatValue = 300.0F), require = 0)
+    private float getMaxPlayerElytraSpeed(float speed) {
+        return Float.MAX_VALUE;
     }
 
-    @ModifyConstant(method = "processVehicleMove", constant = @Constant(doubleValue = 100.0))
-    private double removeVehicleSpeedLimit(double original) {
-        return VERY_HIGH_SPEED_DOUBLE;
+    @ModifyConstant(method = "*", constant = @Constant(doubleValue = 100.0), require = 0)
+    private double getMaxPlayerVehicleSpeed(double speed) {
+        return Double.MAX_VALUE;
     }
 }
